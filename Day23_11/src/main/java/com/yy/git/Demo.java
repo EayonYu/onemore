e49463b92a15d867df8deeec0b9668e17c1c8a97
EayonYu <eayonkyung@163.com>
2017-12-14 16:57:39 +0800

    第一个项目

---
+++ Day23_11/src/main/java/com/yy/git/Demo.java
@@ -0,0 +1,5 @@
+package com.yy.git;
+
+public class Demo {
+
+}
---
+++ Day23_11/src/main/java/com/yy/git/First01.java
@@ -0,0 +1,8 @@
+package com.yy.git;
+
+public class First01 {
+	public static void main(String[] args) {
+		System.out.println("第一个git");
+		System.out.println("dierci");
+	}
+}
